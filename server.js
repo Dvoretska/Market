df2f33fee8f857450fbda9febf83264b28d54807
Anton <it2015maistrenko@gmail.com>
2018-01-14 13:25:22 +0200

    Fix history built router.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var history = require('connect-history-api-fallback');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
+app.use(history());
 app.use(serveStatic(__dirname + "/heroku"));
 var port = process.env.PORT || 5000;
 app.listen(port);
